f524aa30c8baff38595a55b3cae1a72e6236846e
dvdpwll <dvdpwll456@gmail.com>
2016-08-17 10:00:56 -0400

    kalgfoer

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :maps, except: [:new, :edit]
+  resources :maps, except: [:new, :edit]
+  resources :maps, except: [:new, :edit]
+  resources :examples, except: [:new, :edit]
+  post '/sign-up' => 'users#signup'
+  post '/sign-in' => 'users#signin'
+  delete '/sign-out/:id' => 'users#signout'
+  patch '/change-password/:id' => 'users#changepw'
+  resources :users, only: [:index, :show]
+end
