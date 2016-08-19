e6d625c6d2c53bd2a54f102ef508053ef3c63aed
dvdpwll <dvdpwll456@gmail.com>
2016-08-19 11:24:29 -0400

    changed some of the scripts, not super important

--- scripts/post-map.sh
+++ scripts/post-map.sh
@@ -1,4 +1,5 @@
 curl --include --request POST http://localhost:3000/maps \
+  --header "Authorization: Token token=BAhJIiVmOGZmMjUwMTkzNjY5MWVkODkwNGMzYzE3NGI1ZTZjNwY6BkVG--9d510ea89db662a93d2de7d315925ed0cd9ff994" \
   --header "Content-Type: application/json" \
   --data '{
     "map": {
--- scripts/sign-in.sh
+++ scripts/sign-in.sh
@@ -4,7 +4,7 @@ curl --include --request POST http://localhost:3000/sign-in \
   --header "Content-Type: application/json" \
   --data '{
     "credentials": {
-      "email": "a",
-      "password": "a"
+      "email": "an@example.email",
+      "password": "an example password"
     }
   }'
